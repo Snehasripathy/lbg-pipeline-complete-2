5593e2516878202dda1c39b5a505aafcf0397662
Sneha Sripathy <sneha@qa.com>
2024-02-09 11:23:25 +0000

    code changes

--- src/App.js
+++ src/App.js
@@ -44,6 +44,16 @@ function App() {
       </div>
     </div>
   );
+     <div className='header field'>
+      VAT CALCULATOR
+      <div className='colour-border'>
+        <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
+        <PriceEntryField customstyle="field" label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
+        <DisplayBlock customstyle="field" label="VAT to be paid: " value={vatToPay} />
+        <PriceEntryField customstyle="field" label="Price incl VAT: " priceChanged={handleGrossPriceChange} price={grossPrice === 0.0 ? "" : grossPrice} />
+      </div>
+    </div>
+  );
   
 }
 
